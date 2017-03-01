6f7bf4904a5d52e4f61814cbbe6fccde0811a299
Tom Pace <tompace@gmail.com>
2017-03-01 11:18:54 -0500

    Checking for fees due
    ////Checking for fees due (invoiced & non-invoiced) and if so, stopping a permit from being issued internally
    //If there are invoiced fees (balance due), we should not be able to issue a permit (internally)

--- Scripts/Event/WTUB;~!~!~!~.js
+++ Scripts/Event/WTUB;~!~!~!~.js
@@ -17,6 +17,25 @@ if (matches(wfStatus, 'Issue', 'Reissue', 'Issue Partial') && (isTaskActive('HRC
 	cancel = true;
 }
 
+////Checking for fees due (invoiced & non-invoiced) and if so, stopping a permit from being issued internally
+//If there are invoiced fees (balance due), we should not be able to issue a permit (internally)
+if (matches(wfStatus, 'Issue', 'Reissue', 'Issue Partial')) {
+	if (appMatch('Permits/*/*/*') && balanceDue > 0) {
+	                showMessage = true;
+	                comment('<font size=small><b>Balance Due </b></font><br><br>The Balance Due will need to be paid before the permit can be issued.<br><br>');
+	                comment("Balance Due = "+balanceDue);
+	                cancel = true;
+	}
+	//If there are non-invoiced fees, we should not be able to issue a permit (internally)
+	// Note from Keith H -- Check if there a Assessed fees:    Check for NON Invoiced fees
+    newFees = feeGetTotByDateRange("01/01/2000","01/01/2030","NEW"); 
+	if (appMatch('Permits/*/*/*') && newFees > 0) {		        
+	                comment("NEW fee total = "+newFees); comment("Workflow status = "+wfStatus);
+	                showMessage = true; comment("<font size=small><b>Un-Invoiced Fees</b></font><br><br>The permit you are trying to issue or final has assessed fees that have NOT been invoiced. Those fees need to be invoiced or removed prior to the permit being issued or finaled.<br><br>");
+	                cancel = true;
+	}
+}////END of Checking for fees due (invoiced & non-invoiced)
+
 if (wfTask == 'Building' && matches(wfStatus, 'Approved', 'Approved with Conditions', 'Partial Approval') && (getPriority() == null || getPriority() == '')) {
 	cancel = true;
 	showMessage = true;
