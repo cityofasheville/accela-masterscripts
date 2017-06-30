3881543457457e61280a060720aa56467783a32a
Tom Pace <tompace@gmail.com>
2017-06-30 14:30:41 -0400

    Update FAA;~!~!~!~.js
    CFIREADDSC fee update -- less than $10 needs to change to 0

--- Scripts/Event/FAA;~!~!~!~.js
+++ Scripts/Event/FAA;~!~!~!~.js
@@ -1,5 +1,6 @@
+// Added by Jerry for CFIREADDSC fee update -- less than $10 needs to change to 0
 
-if (feeExists('CFIRE') && AInfo['Jurisdiction'] == 'Asheville Zoning Jurisdiction (ETJ)') {
-	updateFee('ETJ','COM_BLD','FINAL',(feeAmount('CFIRE') + feeAmount('CFIREADD'))* .10,'N');
-	}
+if (feeExists('CFIREADDSC') && (feeAmount('CFIREADDSC') < 10)) {
+                updateFee('CFIREADDSC','COM_BLD','FINAL',0,'N')
 
+                }
