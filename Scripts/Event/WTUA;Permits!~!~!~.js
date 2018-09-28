a8df75b92392f60c41393bb3e9233a37bbae77d4
mmazanec22 <melanie.mazanec@gmail.com>
2018-09-28 10:47:14 -0400

    Change the third iteration

--- Scripts/Event/WTUA;Permits!~!~!~.js
+++ Scripts/Event/WTUA;Permits!~!~!~.js
@@ -471,10 +471,10 @@ if ((appMatch('Planning/Development/*/*')
 	&& AInfo['Issue Grading Permit To'] != 'NA'
 ) {
 	emailContact('Grading Preliminary Inspection Required', 'Permit Number: ' + capIDString + ' <br> Location: ' + CapAddress + ' <br> A Grading Permit is being issued for this location contingent upon proper installation of all erosion control devices. <br> The contractor must schedule a GR-PRELIM inspection before commencing work. All approved plans and permits must be on site. Silt Fencing and a Construction Entrance must be installed, and the temporary address must be posted. This inspection is required on individual lots, master sites and also required for individual lots within Subdivisions. Building permit inspections will be delayed until the Grading Preliminary inspection is approved. Thank you.');
-	if (profArr != null) {
-		for (x in profArr) {
-			if (profArr[x].getEmail() + '' != '') {
-				email(profArr[x].getEmail(), 'noreply@ashevillenc.gov', 'Grading Preliminary Inspection Required', 'Permit Number: ' + capIDString + ' <br> Location: ' + CapAddress + ' <br> A Grading Permit is being issued for this location contingent upon proper installation of all erosion control devices. <br> The contractor must schedule a GR-PRELIM inspection before commencing work. All approved plans and permits must be on site. Silt Fencing and a Construction Entrance must be installed, and the temporary address must be posted. This inspection is required on individual lots, master sites and also required for individual lots within Subdivisions. Building permit inspections will be delayed until the Grading Preliminary inspection is approved. Thank you.');
+	if (profArr !== null) {
+		for (profObj3 in profArr) {
+			if (profObj3.getEmail() + '' !== '') {
+				email(profObj3.getEmail(), 'noreply@ashevillenc.gov', 'Grading Preliminary Inspection Required', 'Permit Number: ' + capIDString + ' <br> Location: ' + CapAddress + ' <br> A Grading Permit is being issued for this location contingent upon proper installation of all erosion control devices. <br> The contractor must schedule a GR-PRELIM inspection before commencing work. All approved plans and permits must be on site. Silt Fencing and a Construction Entrance must be installed, and the temporary address must be posted. This inspection is required on individual lots, master sites and also required for individual lots within Subdivisions. Building permit inspections will be delayed until the Grading Preliminary inspection is approved. Thank you.');
 			}
 		}
 	}
