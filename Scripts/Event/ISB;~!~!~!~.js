42f00ce286721d024106204483c99b7de704919f
mmazanec22 <melanie.mazanec@gmail.com>
2018-10-05 10:30:10 -0400

    Comments, fix concatenation

--- Scripts/Event/ISB;~!~!~!~.js
+++ Scripts/Event/ISB;~!~!~!~.js
@@ -544,16 +544,17 @@ for (var closeOutObjectIndex = 0; closeOutObjectIndex < closeOutDocuments.length
 }
 /********** end CLOSEOUT STUFF **********/
 
-if (matches(inspType, 'ZO-FINAL') && (appMatch('Permits/Commercial/Site Work/NA') || appMatch('Planning/Development/*/*') || appMatch('Planning/Subdivision/*/*'))) {
-
+if (matches(inspType, 'ZO-FINAL')
+	&& (appMatch('Permits/Commercial/Site Work/NA')
+		|| appMatch('Planning/Development/*/*')
+		|| appMatch('Planning/Subdivision/*/*')
+	)
+) {
 	//start replaced branch: ES_ISB_ZONG
-	{
-		if (checkInspectionResult('FP-SITE FINAL', 'Pending')) {
-			showMessage = true;
-			comment("<font size=small><b>Can't schedule Final:</b></font><br><br>Can't schedule Final until FP-SITE FINAL is scheduled.<br><br>");
-			cancel = true;
-		}
-
+	if (checkInspectionResult('FP-SITE FINAL', 'Pending')) {
+		showMessage = true;
+		comment("<font size=small><b>Can't schedule Final:</b></font><br><br>Can't schedule Final until FP-SITE FINAL is scheduled.<br><br>");
+		cancel = true;
 	}
 	//end replaced branch: ES_ISB_ZONG;
 }
@@ -562,17 +563,14 @@ pCapId = getParent();
 
 if (pCapId) {
 	//start replaced branch: ES_CHECK_FOR_BALANCE_ON_PARENT
-	{
-		pCapDetailObjResult = aa.cap.getCapDetail(pCapId);
-		pCapDetail = pCapDetailObjResult.getOutput();
-		pBalanceDue = pCapDetail.getBalance();
-		comment('Balance is ' + pBalanceDue);
-		if (pBalanceDue > 0) {
-			showMessage = true;
-			comment("<font size=small><b>Parent Permit Has Balance Due:</b></font><br><br>The parent permit has a balance due of $'+pBalanceDue+'.  Inspections cannot be scheduled.<br><br>");
-			cancel = true;
-		}
-
+	pCapDetailObjResult = aa.cap.getCapDetail(pCapId);
+	pCapDetail = pCapDetailObjResult.getOutput();
+	pBalanceDue = pCapDetail.getBalance();
+	comment('Balance is ' + pBalanceDue);
+	if (pBalanceDue > 0) {
+		showMessage = true;
+		comment("<font size=small><b>Parent Permit Has Balance Due:</b></font><br><br>The parent permit has a balance due of $" + pBalanceDue + ".  Inspections cannot be scheduled.<br><br>");
+		cancel = true;
 	}
 	//end replaced branch: ES_CHECK_FOR_BALANCE_ON_PARENT;
 }
