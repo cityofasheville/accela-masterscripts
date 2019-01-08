1564af680e2648b7cefa7f245d249fce949feea6
Melanie Mazanec <melanie.mazanec@gmail.com>
2019-01-08 10:53:23 -0500

    Spacing

--- Scripts/Event/WTUA;Permits!~!~!~.js
+++ Scripts/Event/WTUA;Permits!~!~!~.js
@@ -521,9 +521,9 @@ if ((appMatch('Planning/Development/*/*') || appMatch('Planning/Subdivision/*/*'
 
 //test
 var revisionRequiredEmailSubject = 'Action Needed Regarding Your Permit';
-var revisionRequiredEmailText = 		'Your plan review is complete for the permit ' + capIDString + '. The plan requires revision. You may pick up your marked up plan at https://services.ashevillenc.gov/CitizenAccess . Login and search for your permit, then go to Record Info>Attachments to download the markup and comments. After you have completed the revision, please resubmit that at our site https://develop.residentialplans.ashevillenc.gov/ If you have questions please contact the Permit Application Center at PAC@ashevillenc.gov or 828-259-5846.'
+var revisionRequiredEmailText = 'Your plan review is complete for the permit ' + capIDString + '. The plan requires revision. You may pick up your marked up plan at https://services.ashevillenc.gov/CitizenAccess . Login and search for your permit, then go to Record Info>Attachments to download the markup and comments. After you have completed the revision, please resubmit that at our site https://develop.residentialplans.ashevillenc.gov/ If you have questions please contact the Permit Application Center at PAC@ashevillenc.gov or 828-259-5846.';
 var contactArray = getContactArray(capId).map(function(contact) {
-	return contact.getEmail()
+	return contact.getEmail();
 })
 
 
