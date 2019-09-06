a6ea8ba0c1729a1ab5ea5356ef7715cb936534f8
Jon Wilson <jontwilson@gmail.com>
2019-09-06 09:12:49 -0400

    9/5

---
+++ INCLUDES_CUSTOM/emailByContactType.js
@@ -0,0 +1,30 @@
+function emailByContactType(emailSubj, emailBody, toAddr, fromAddr, contactType) {
+    toAddr = typeof toAddr !== 'undefined' ? toAddr : "";
+    fromAddr = typeof fromAddr !== 'undefined' ? fromAddr : "developmentservices@ashevillenc.gov";
+    contactType = typeof contactType !== 'undefined' ? contactType : "";
+    var emailAddrs = [];
+
+    if(toAddr != "") { emailAddrs.push(toAddr); }
+
+    if(contactType != "") {
+      var CapContacts = aa.people.getCapContactByCapID(capId);
+      if (CapContacts.getSuccess()) {
+        var ContactOutputs = CapContacts.getOutput();
+        for (yy in ContactOutputs) {
+          if(contactType.equals(ContactOutputs[yy].getCapContactModel().getPeople().getContactType())) { 
+            if(ContactOutputs[yy].getEmail() != null) {
+              emailAddrs.push(ContactOutputs[yy].getEmail());
+            }
+          }
+        }
+      }
+    }
+
+    var emailString = emailAddrs.join(';') 
+    if(emailString.indexOf("@") > 0) {
+      aa.sendMail(fromAddr, emailString, toAddr, emailSubj, emailBody); 
+      logDebug("Successfully sent emails");
+    }else{
+      logDebug("Couldn't send emails, invalid address");
+    }
+}
---
+++ INCLUDES_CUSTOM/emailByLicenseType.js
@@ -0,0 +1,36 @@
+function emailByLicenseType(emailSubj, emailBody, toAddr, fromAddr, licenseType) {
+  toAddr = typeof toAddr !== 'undefined' ? toAddr : "";
+  fromAddr = typeof fromAddr !== 'undefined' ? fromAddr : "developmentservices@ashevillenc.gov";
+  licenseType = typeof licenseType !== 'undefined' ? licenseType : "";
+  var emailAddrs = [];
+
+  if(toAddr != "") { emailAddrs.push(toAddr); }
+
+  if(contactType != "") {
+    var CapContacts = aa.people.getCapContactByCapID(capId);
+    if (CapContacts.getSuccess()) {
+      var ContactOutputs = CapContacts.getOutput();
+      for (yy in ContactOutputs) {
+        if(contactType.equals(ContactOutputs[yy].getCapContactModel().getPeople().getContactType())) { 
+          if(ContactOutputs[yy].getEmail() != null) {
+            emailAddrs.push(ContactOutputs[yy].getEmail());
+          }
+        }
+      }
+    }
+  }
+
+  //var profArr = getLicenseProfessional(capId);
+  //if (profArr != null) {
+  //	for(x in profArr) if(profArr[x].getEmail() + '' != '') email(profArr[x].getEmail(),'noreply@ashevillenc.gov','Inspection Resulted','You are a professional on permit '+capIDString+' An Inspection '+inspType+' was completed with a result of '+inspResult+'.<br>Inspection Comment: '+inspComment+'<br><br><br>Thank You.');
+  //	}
+
+  var emailString = emailAddrs.join(';') 
+  if(emailString.indexOf("@") > 0) {
+    aa.sendMail(fromAddr, emailString, toAddr, emailSubj, emailBody); 
+    logDebug("Successfully sent emails");
+  }else{
+    logDebug("Couldn't send emails, invalid address");
+  }
+}
+// TO CALL: emailByLicenseType(emailSubj, emailBody, toAddr, fromAddr, licenseType)
