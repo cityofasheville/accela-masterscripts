657b361ac4f2aa3c693290f618c8d82b9b435654
Jon Wilson <jtwilson@ashevillenc.gov>
2019-09-10 08:38:08 -0400

    licenseType

--- INCLUDES_CUSTOM/emailByLicenseType.js
+++ INCLUDES_CUSTOM/emailByLicenseType.js
@@ -29,7 +29,8 @@ function emailByLicenseType(emailSubj, emailBody, licenseType, fromAddr, toAddr)
   for (iProf in profObjArray) {
     var tProfObj = profObjArray[iProf];
 //    logDebug("LP Name: " + tProfObj.people.getFirstName() + " " + tProfObj.people.getLastName());
-    var vProfObj = new licenseProfObject(tProfObj.getLicenseNbr()); // refLicModel,infoTableGroupCodeObj,infoTableSubGroupCodesObj,infoTables,attribs,valid,validTables,validAttrs,getEmailTemplateParams,refreshTables,getMaxRowByTable,addTableRow,addTableFromASIT,removeTableRow,removeTable,setTableEnabledFlag,setDisplayInACA4Table,getAttribute,setAttribute,updateFromRecordContactByType,updateFromAddress,updateFromRecordLicensedProf,copyToRecord,enable,disable,getAssociatedRecords,updateRecord
+    var vProfObj = new licenseProfObject(tProfObj.getLicenseNbr()); 
+    // refLicModel,infoTableGroupCodeObj,infoTableSubGroupCodesObj,infoTables,attribs,valid,validTables,validAttrs,getEmailTemplateParams,refreshTables,getMaxRowByTable,addTableRow,addTableFromASIT,removeTableRow,removeTable,setTableEnabledFlag,setDisplayInACA4Table,getAttribute,setAttribute,updateFromRecordContactByType,updateFromAddress,updateFromRecordLicensedProf,copyToRecord,enable,disable,getAssociatedRecords,updateRecord
     
     var keysProfObj = Object.keys(vProfObj);
     logDebug("keysProfObj: " + keysProfObj);
@@ -37,6 +38,35 @@ function emailByLicenseType(emailSubj, emailBody, licenseType, fromAddr, toAddr)
     var refLicModel = vProfObj.refLicModel;
     var keysRefLicModel = Object.keys(refLicModel);
     logDebug("keysRefLicModel: " + keysRefLicModel);
+
+    var licenseType = vProfObj.refLicModel.getLicenseType();
+    logDebug("licenseType: " + licenseType);
+
+// hashCode,acaPermission,wait,auditID,setContactLastName,businessLicense,getCountry,setBusinessName2,getContactLastName,setAddress3,
+// businessName2,setAddress2,wcPolicyNo,setSuffixName,selfIns,getAuditID,getPolicy,setCountryCode,getAuditStatus,getSocialSecurityNumber,
+// getEMailAddress,setSocialSecurityNumber,setWcSuspendDate,insuranceAmount,setLicenseExpirationDate,getContactMiddleName,setContrLicNo,
+// getLicenseIssueDate,setEinSs,setWcPolicyNo,getComment,wcCancDate,licenseLastRenewalDate,getInfoTableGroupCodeModel,setLicState,
+// getWcExempt,getFein,setContactMiddleName,setPolicy,recLocd,getClass,getPhone2CountryCode,setAddress1,setWcExempt,setInsuranceAmount,
+// setRecLocd,setLicenseBoard,getFax,setServiceProviderCode,getLicenseType,setAuditDate,insuranceCo,setWcInsCoCode,
+// peopleAttributeScriptModels,getState,setWcIntentToSuspNtcSentDate,cityCode,getCountryCode,address1,phone2,address2,
+// setInsuranceExpDate,phone3,address3,getPermStatusCode,phone1,holdDesc,getAuditDate,contLicBusName,getLicenseExpirationDate,
+// getAcaPermission,licSeqNbr,setInfoTableGroupCodeModel,permStatusCode,getContryCode,setAttributes,wcInsCoCode,fax,setWcExpDate,
+// getWcPolicyNo,socialSecurityNumber,countryCode,setHoldCode,country,getLicState,contrLicNo,getLicenseModel,getLicSeqNbr,einSs,
+// wcSuspendDate,getPhone2,getBusinessLicense,getPhone3,getPhone1,setLicenseIssueDate,phone3CountryCode,setFax,getMaskedSsn,
+// licenseExpirationDate,licOrigIssDate,setPhone3,setPhone2,getLicenseBoard,businessLicExpDate,setPhone1,setMaskedSsn,getWcEffDate,
+// setAcaPermission,getHoldCode,getPhone1CountryCode,getZip,getPhone3CountryCode,infoTableGroupCodeModel,getInsuranceAmount,
+// getPeopleAttributeScriptModels,setEMailAddress,setAgencyCode,contactLastName,setCity,contactFirstName,setLicenseLastRenewalDate,
+// wcExpDate,getEinSs,licenseIssueDate,setPhone2CountryCode,attributes,getTypeFlag,setComment,setContryCode,setBusinessLicExpDate,
+// stateLicense,setCountry,setLicSeqNbr,city,wcIntentToSuspNtcSentDate,serviceProviderCode,setWcCancDate,licenseType,equals,getSelfIns,
+// phone2CountryCode,getBusinessName,setAuditStatus,comment,setCityCode,EMailAddress,getAddress3,getAddress2,getAddress1,setHoldDesc,
+// typeFlag,maskedSsn,getContactFirstName,recSecurity,licState,getRecLocd,fein,getCity,getWcIntentToSuspNtcSentDate,setWcEffDate,
+// wcExempt,getCityCode,agencyCode,getLicenseLastRenewalDate,getWcInsCoCode,getRecSecurity,insuranceExpDate,getServiceProviderCode,
+// auditDate,getContLicBusName,setFaxCountryCode,setStateLicense,setLicenseType,setTypeFlag,setBusinessName,businessName,getAgencyCode,
+// getBusinessLicExpDate,setBusinessLicense,getHoldDesc,getSuffixName,getFaxCountryCode,getInsuranceExpDate,auditStatus,setLicOrigIssDate,
+// setContactFirstName,notifyAll,licenseBoard,setAuditID,faxCountryCode,getLicOrigIssDate,licenseModel,class,toString,policy,
+// getAttributes,phone1CountryCode,getBusinessName2,state,setRecSecurity,getWcExpDate,contactMiddleName,getStateLicense,notify,
+// getWcSuspendDate,setState,setSelfIns,holdCode,getInsuranceCo,suffixName,zip,setInsuranceCo,getContrLicNo,getWcCancDate,
+// setContLicBusName,setPermStatusCode,setFein,setZip,wcEffDate,setPhone3CountryCode,setPhone1CountryCode,contryCode
 
     
     // logDebug("LP Email: " + vProfObj.refLicModel.getEMailAddress());
