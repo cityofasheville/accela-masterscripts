4cce900e7e9ef8f34eb98252aa73f4c02762d74f
Jon Wilson <jontwilson@gmail.com>
2019-12-03 14:18:19 -0500

    Update ISA;~!~!~!~.js

--- Scripts/Event/ISA;~!~!~!~.js
+++ Scripts/Event/ISA;~!~!~!~.js
@@ -1,6 +1,5 @@
 theCapAddress = getCapAddress(capId);
 
-
 if (matches(inspType, 'PL-ROUGH IN') && isTaskActive('Backflow Approval') && !appMatch('Permits/Commercial/Existing Building/Repair-Replacement') && (appMatch('Permits/Commercial/Existing Building/*') || appMatch('Permits/Commercial/New Building/*'))) {
 //TODO: CapAdress undefined
 	email('backflowmailbox@ashevillenc.gov', 'noreply@ashevillenc.gov', 'Inspection Request', 'A plumbing rough-in inspection has been scheduled for Permit Application ' + capIDString + ' at address ' + CapAddress + '. Please inspect the backflow device and email the Permit Application Center general mailbox your results.');
