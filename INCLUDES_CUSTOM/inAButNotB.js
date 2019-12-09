6fd26c9da6ec864365e05ff14df7ac0bef62e450
Jon Wilson <jontwilson@gmail.com>
2019-12-09 12:33:54 -0500

    debug

--- INCLUDES_CUSTOM/inAButNotB.js
+++ INCLUDES_CUSTOM/inAButNotB.js
@@ -1,6 +1,8 @@
 function inAButNotB(a1, a2) {
   // given two arrays, returns everything that appears in first list but not the second.
   // Use this to send emails to everyone in first list unless they are in second.
+  // The toString funcs are needed because Accela funcs don't return JS strings, they return [object JavaObject] 
+  // https://developer.mozilla.org/en-US/docs/Archive/Web/LiveConnect_Reference/JavaObject "Archive of obsolete content"
 
   var result = [];
   for (var i = 0; i < a1.length; i++) {
@@ -11,5 +13,3 @@ function inAButNotB(a1, a2) {
   return result;
 }
 
-  console.log(inAButNotB(['a ','b','c'], ['a']));
-  console.log(inAButNotB(['a','b','c'], ['a ']));
