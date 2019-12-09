2da124a72e6d97081c99dae1a479a3e69e0409e3
Jon Wilson <jontwilson@gmail.com>
2019-12-09 08:57:42 -0500

    debug

--- Scripts/Event/WTUA;Permits!~!~!~.js
+++ Scripts/Event/WTUA;Permits!~!~!~.js
@@ -708,6 +708,7 @@ function inAButNotB_loc(a1, a2) {
   comment(a2);
   comment('a1.length= ' + a1.length);
   comment('a1.typeof= ' + a1.typeof);
+  comment(Array.isArray(a1));
   for (var i = 0; i < a1.length; i++) {
     comment('i= ' + i);
 
