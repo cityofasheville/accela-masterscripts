3f2d00178dc85b1bc1a4fa1af0e44316bc24537c
Jon Wilson <jontwilson@gmail.com>
2020-01-02 14:59:23 -0500

    createPendingInspectionIfNotExists function

--- INCLUDES_CUSTOM/createPendingInspectionIfNotExists.js
+++ INCLUDES_CUSTOM/createPendingInspectionIfNotExists.js
@@ -1,6 +1,6 @@
 function createPendingInspectionIfNotExists(insGrp, insType) {
   // only creates the pending inspection if it doesn't already exist
-  if(checkInspectionResult(insType, 'Pending')){
+  if(!checkInspectionResult(insType, 'Pending')){
     createPendingInspection(insGrp, insType);
   }
 }
