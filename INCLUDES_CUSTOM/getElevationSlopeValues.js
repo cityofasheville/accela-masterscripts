59b6392937042217de4b5df7dbd6822165d61d4c
Jon Wilson <jtwilson@ashevillenc.gov>
2020-04-02 10:38:15 -0400

    testing commenting out slope code

--- INCLUDES_CUSTOM/getElevationSlopeValues.js
+++ INCLUDES_CUSTOM/getElevationSlopeValues.js
@@ -9,20 +9,20 @@ function getElevationSlopeValues(parcelNumber)
    // initializing the return array
    returnArray["maxElevation"] = null;
    returnArray["percentSlope"] = null;
-   var aURL = "https://mapwnc.org/api/slopebypin/" + parcelNumber;
-   //var aURL = "https://coa-bc-steep-slope-calculator.herokuapp.com/api/slopebypin/" + parcelNumber;
-   // aa.print(aURL);
-   var vOutObj = aa.httpClient.get(aURL);
+   // var aURL = "https://mapwnc.org/api/slopebypin/" + parcelNumber;
+   // //var aURL = "https://coa-bc-steep-slope-calculator.herokuapp.com/api/slopebypin/" + parcelNumber;
+   // // aa.print(aURL);
+   // var vOutObj = aa.httpClient.get(aURL);
 
-   if(vOutObj.getSuccess())
-   {
-      var vOut = vOutObj.getOutput();
-      //  aa.print(vOut);
-      // not sure if we need this JSON.parse, getOutput might do this already
-      var vOutParsed = JSON.parse(vOut);
-      returnArray["maxElevation"] = vOutParsed.maxElevation;
-      returnArray["percentSlope"] = vOutParsed.percentSlope;
-   }
+   // if(vOutObj.getSuccess())
+   // {
+   //    var vOut = vOutObj.getOutput();
+   //    //  aa.print(vOut);
+   //    // not sure if we need this JSON.parse, getOutput might do this already
+   //    var vOutParsed = JSON.parse(vOut);
+   //    returnArray["maxElevation"] = vOutParsed.maxElevation;
+   //    returnArray["percentSlope"] = vOutParsed.percentSlope;
+   // }
 
    return returnArray;
 }
