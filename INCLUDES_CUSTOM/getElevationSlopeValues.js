58e984e163b22b34129ebc239bc7f8f248fffe81
Jon Wilson <jontwilson@gmail.com>
2020-04-02 12:41:41 -0400

    try hard code with quotes

--- INCLUDES_CUSTOM/getElevationSlopeValues.js
+++ INCLUDES_CUSTOM/getElevationSlopeValues.js
@@ -20,8 +20,8 @@ function getElevationSlopeValues(parcelNumber)
       //  aa.print(vOut);
       // not sure if we need this JSON.parse, getOutput might do this already
       var vOutParsed = JSON.parse(vOut);
-      returnArray["maxElevation"] = vOutParsed.maxElevation;
-      returnArray["percentSlope"] = vOutParsed.percentSlope;
+      returnArray["maxElevation"] = "2135.00"; // vOutParsed.maxElevation;
+      returnArray["percentSlope"] = "9.30"; // vOutParsed.percentSlope;
    }
 
    return returnArray;
