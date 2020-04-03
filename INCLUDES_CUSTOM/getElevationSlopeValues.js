4c38f50f22831947d430f98ad29fa19af24af3bf
Jon Wilson <jtwilson@ashevillenc.gov>
2020-04-03 07:36:15 -0400

    another test

--- INCLUDES_CUSTOM/getElevationSlopeValues.js
+++ INCLUDES_CUSTOM/getElevationSlopeValues.js
@@ -1,38 +1,34 @@
-// function getElevationSlopeValues(parcelNumber)
-// {
-//    if( ! parcelNumber) // not passed a valid string or number return false
-//    {
-//       logDebug("Function getElevationSlopeValues requires a valid String or Integer passed");
-//      return false;
-//    }
-//    var returnArray = new Array();
-//    // initializing the return array
-//    returnArray["maxElevation"] = null;
-//    returnArray["percentSlope"] = null;
-//    // var aURL = "https://mapwnc.org/api/slopebypin/" + parcelNumber;
-//    var aURL = "https://coa-bc-steep-slope-calculator.herokuapp.com/api/slopebypin/" + parcelNumber;
-//    // aa.print(aURL);
-//    var vOutObj = aa.httpClient.get(aURL);
-
-//    if(vOutObj.getSuccess())
-//    {
-//       var vOut = vOutObj.getOutput();
-//       //  aa.print(vOut);
-//       // not sure if we need this JSON.parse, getOutput might do this already
-//       var vOutParsed = JSON.parse(vOut);
-//       returnArray["maxElevation"] = "2135.00"; // vOutParsed.maxElevation;
-//       returnArray["percentSlope"] = "9.30"; // vOutParsed.percentSlope;
-//    }
-
-//    return returnArray;
-// }
 function getElevationSlopeValues(parcelNumber)
 {
+   if( ! parcelNumber) // not passed a valid string or number return false
+   {
+      logDebug("Function getElevationSlopeValues requires a valid String or Integer passed");
+     return false;
+   }
    var returnArray = new Array();
-   returnArray["maxElevation"] = "2135.00"; 
-   returnArray["percentSlope"] = "9.30"; 
+   // initializing the return array
+   returnArray["maxElevation"] = null;
+   returnArray["percentSlope"] = null;
+   // var aURL = "https://mapwnc.org/api/slopebypin/" + parcelNumber;
+   var aURL = "https://coa-bc-steep-slope-calculator.herokuapp.com/api/slopebypin/" + parcelNumber;
+   // aa.print(aURL);
+   var vOutObj = aa.httpClient.get(aURL);
+
+   // if(vOutObj.getSuccess())
+   {
+      var vOut = vOutObj.getOutput();
+      //  aa.print(vOut);
+      // not sure if we need this JSON.parse, getOutput might do this already
+      var vOutParsed = JSON.parse(vOut);
+      vOutParsed = {"maxElevation": "2135.00", "percentSlope": "9.30"};
+      aa.print(vOutParsed);
+      returnArray["maxElevation"] = vOutParsed.maxElevation;
+      returnArray["percentSlope"] = vOutParsed.percentSlope;
+   }
+
    return returnArray;
 }
+
 
 
 
