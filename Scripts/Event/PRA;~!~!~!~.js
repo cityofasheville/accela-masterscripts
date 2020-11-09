8246fe98c5897c54551d0c107f44b2fa47bcb7fc
Jon <jtwilson@ashevillenc.gov>
2020-11-09 07:23:19 -0500

    pra didnt work

--- Scripts/Event/PRA;~!~!~!~.js
+++ Scripts/Event/PRA;~!~!~!~.js
@@ -9,24 +9,24 @@ if (appMatch('Services/Project Inquiry/Meeting Request/NA') && balanceDue == 0) 
 
 // 11/06/2020
 // Home Stay payment received instruction email
-if (publicuser && appMatch('*/*/*/Home Stay')) {
-	var emailSubj = "Payment Received -- Instructions for Scheduling Your Inspection"
-	var emailBody = '<html><head><style>ol {margin: 0;padding: 0}</style></head><body>Permit Number: ' 
-	+ capIDString + ' <br>Location: ' + CapAddress
-	+ '<br><p>'
-	+ ' Hello, we have received your payment and your homestay renewal application has been approved, pending inspection. '
-	+ ' Please click the link below to schedule your inspection: '
-	+ '</p><p>'
-	+ ' <a href="https://www.jotform.com/build/202533942220142">https://www.jotform.com/build/202533942220142</a>'
-	+ '</p><p>'
-	+ ' <b>The inspection must be completed within 30 days of this email or your homestay permit will be revoked.</b> '
-	+ ' <span style="color:red;">The inspection will be held online using Google Meet.</span>'
-	+ ' You will receive instructions for accessing this meeting in an email confirming your inspection time. '
-	+ '</p><p>'
-	+ ' A one year homestay renewal permit will be issued once your property passes this remote inspection. '
-	+ '</p><p>'
-	+ 'If you have any questions, please contact Haley Mahoney at hmahoney@ashevillenc.gov.  Thank you.'
-	+ ' </p><hr></body></html>'
-	emailAllContacts(emailSubj, emailBody)
-}	
+// if (publicuser && appMatch('*/*/*/Home Stay')) {
+// 	var emailSubj = "Payment Received -- Instructions for Scheduling Your Inspection"
+// 	var emailBody = '<html><head><style>ol {margin: 0;padding: 0}</style></head><body>Permit Number: ' 
+// 	+ capIDString + ' <br>Location: ' + CapAddress
+// 	+ '<br><p>'
+// 	+ ' Hello, we have received your payment and your homestay renewal application has been approved, pending inspection. '
+// 	+ ' Please click the link below to schedule your inspection: '
+// 	+ '</p><p>'
+// 	+ ' <a href="https://www.jotform.com/build/202533942220142">https://www.jotform.com/build/202533942220142</a>'
+// 	+ '</p><p>'
+// 	+ ' <b>The inspection must be completed within 30 days of this email or your homestay permit will be revoked.</b> '
+// 	+ ' <span style="color:red;">The inspection will be held online using Google Meet.</span>'
+// 	+ ' You will receive instructions for accessing this meeting in an email confirming your inspection time. '
+// 	+ '</p><p>'
+// 	+ ' A one year homestay renewal permit will be issued once your property passes this remote inspection. '
+// 	+ '</p><p>'
+// 	+ 'If you have any questions, please contact Haley Mahoney at hmahoney@ashevillenc.gov.  Thank you.'
+// 	+ ' </p><hr></body></html>'
+// 	emailAllContacts(emailSubj, emailBody)
+// }	
 
