7e71dc3e7bfc0d37c9ebbb13e8744458097730c8
SYSTEM <SYSTEM@CoA-Accela-Prod.asheville.local>
2022-06-10 10:05:17 -0400

    commit by emse tools 2022-06-10 10:05:17

--- Scripts/Event/PRA;~!~!~!~.js
+++ Scripts/Event/PRA;~!~!~!~.js
@@ -7,30 +7,26 @@ if (appMatch('Services/Project Inquiry/Meeting Request/NA') && balanceDue == 0) 
 	email('hmahoney@ashevillenc.gov','noreply@ashevillenc.gov','Early Assistance '+capIDString+' Payment Applied','A payment has been applied on the Early Assistance record '+capIDString+' and it requires your attention.');
 	}
 
-// 11/06/2020 modified 12/2 by Jerry to fix publicUser and uncommented
-// Home Stay payment received instruction email
+// 4/27/22 jtwilson New version
  if (publicUser && appMatch('*/*/*/Home Stay')) {
  	var emailSubj = "Payment Received -- Instructions for Scheduling Your Inspection"
  	var emailBody = '<html><head><style>ol {margin: 0;padding: 0}</style></head><body>Permit Number: ' 
  	+ capIDString + ' <br>Location: ' + CapAddress
  	+ '<br><p>'
- 	+ ' Hello, we have received your payment and your homestay renewal application has been approved, pending inspection. '
- 	+ ' Please click the link below to schedule your inspection: '
- 	+ '</p><p>'
- 	+ ' <a href="https://www.jotform.com/build/202533942220142">https://www.jotform.com/build/202533942220142</a>'
-	+ '</p><p>'
-	
-	+ '<b>This will be a <span style="color:red">REMOTE</span> (virtual) inspection and must be completed within '
-	+ '30 days of this email or your homestay permit will be revoked. </b>'
-	+ ' <span style="color:red;">The inspection will be held online using Google Meet.</span>'
-	+ '  You will receive instructions for accessing this meeting in an email confirming your inspection time. '
-	+ '</p><p>'
 
- 	+ ' A one year homestay renewal permit will be issued once your property passes this remote inspection. '
-	 + '</p><p>'
-	 	 
- 	+ 'If you have any questions, please contact Haley Mahoney at hmahoney@ashevillenc.gov.  Thank you.'
-	 + ' </p><hr></body></html>'
+	+ ' Hello, we have received your payment and your homestay application has been approved, pending inspection. Please click the link below to schedule your inspection: '
+	+ '</p><p>'
+	+ ' <a href="https://www.jotform.com/build/202533942220142">https://www.jotform.com/build/202533942220142</a>'
+	+ '</p><p>'
+	+ '<span style="color:red">'
+	+ ' This will be an on-site inspection of the homestay area. The primary permit applicant must be present at the inspection.  '
+	+ '</span>'
+	+ '</p><p>'
+	+ ' A homestay permit will be issued once your property passes this remote inspection, and will be valid until you wish to close the permit. '
+	+ ' <b>If anything changes with ownership, layout, or hosting of the homestay, please contact homestayinspections@ashevillenc.gov to discuss how to document these changes. </b>' 
+	+ '</p><p>'
+	+ ' If you have any questions, please reach out. Thank you. '
+	+ ' </p><hr></body></html>'
 	 var staffEmail = 'hmahoney@ashevillenc.gov'
  	emailAllContacts(emailSubj, emailBody, staffEmail)
  }	
