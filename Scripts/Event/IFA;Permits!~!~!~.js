25fd2ab9906e059f01d0c28d2be287f1940cf0f7
Lennon Bass <lbass@ashevillenc.gov>
2022-08-23 11:55:19 -0400

    invoice notification

--- Scripts/Event/IFA;Permits!~!~!~.js
+++ Scripts/Event/IFA;Permits!~!~!~.js
@@ -4,6 +4,7 @@ if (appMatch('Permits/*/*/*') ) {
 	var emailParams = aa.util.newHashtable();
 	addParameter (emailParams, "$$firstName$$", applicant.name);
 	addParameter (emailParams, "$$RecordUrl$$", recordURL);
+	addParameter (emailParams, "$$CapID$$", capIDString);
 
 	sendNotification("noreply@ashevillenc.gov",applicant.email,"","INVOICE_NOTIFICATION",emailParams,null);
 	
