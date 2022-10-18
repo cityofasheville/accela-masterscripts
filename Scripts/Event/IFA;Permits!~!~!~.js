2293354f6b729e4e789681f56d0a779a57339672
SYSTEM <SYSTEM@CoA-Accela-Prod.asheville.local>
2022-10-18 10:33:23 -0400

    commit by emse tools 2022-10-18 10:33:23

--- Scripts/Event/IFA;Permits!~!~!~.js
+++ Scripts/Event/IFA;Permits!~!~!~.js
@@ -1,11 +1,1 @@
-if (appMatch('Permits/*/*/*') ) {
-	var applicant = getApplicantInfo(capId);
-	var recordURL = getACAUrl(capId);
-	var emailParams = aa.util.newHashtable();
-	addParameter (emailParams, "$$firstName$$", applicant.name);
-	addParameter (emailParams, "$$RecordUrl$$", recordURL);
-	addParameter (emailParams, "$$CapID$$", capIDString);
-
-	sendNotification("noreply@ashevillenc.gov",applicant.email,"","INVOICE_NOTIFICATION",emailParams,null);
-	
-}
+// Enter your script here...
